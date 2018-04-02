bcd38993d8b2173ab6121b5ef55569550d329bd1
LeeSD90 <lee.doyle90@gmail.com>
2018-04-02 21:07:00 +0200

    Added play and take_turn methods

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,8 +1,41 @@
 class ConnectFour
-	attr_accessor :board
+	attr_accessor :board, :p1, :p2
+	@win = false
+	@last_turn
 
 	def initialize
 		@board = Board.new
+		@p1 = Player.new("Player One", "Red")
+		@p2 = Player.new("Player Two", "Yellow")
+		@last_turn = @p2
+		play
+	end
+
+	def play
+		until @win
+			@last_turn == @p1 ? take_turn(@p2) : take_turn(@p1)
+			return if @win
+		end
+	end
+
+	def take_turn(player)
+		puts "Your turn #{player.name}."
+		x, y = -1
+
+		loop do
+			puts "Select a column(1-7) with an empty space."
+			x = gets.chomp.to_i
+			y = board.count_column(x) + 1
+			if @board.set_cell(x,y,player.color)
+				break
+			end
+		end
+
+		@last_turn = player
+		@win = check_win
+	end
+
+	def check_win
 	end
 
 end
@@ -22,22 +55,39 @@ class Board
 	end
 
 	def set_cell(x, y, v)
-		if !x.between?(1,6) || !y.between?(1,7) then
-			return "Cell (#{x}, #{y}) is out of bounds!"
+		if !x.between?(1,7) || !y.between?(1,6) then
+			return false
 		elsif v != "Red" && v!= "Yellow"
-			return "Value #{v} is invalid!"
+			return false
 		else
 			@cells[x][y] = v
+			return true
+		end
+	end
+
+	def count_column(x)
+		if !x.between?(1,7) then
+			return "Column #{x} doesn't exist!"
+		else
+			return cells[x].length - cells[x].count(nil)
 		end
 	end
 
 	private
 
 	def create_board
-		return Array.new(6){Array.new(7)}
+		return Array.new(7){Array.new(6)}
 	end
 
 end
 
 class Player
+	attr_accessor :name, :color
+
+	def initialize(player_name, color)
+		@name = player_name
+		@color = color
+	end
 end
+
+game = ConnectFour.new
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,17 +3,15 @@ require 'connect_four'
 describe ConnectFour do
 	let(:game) {ConnectFour.new}
 
-	describe "Player" do
+	describe '#check_win' do
 	end
 
 	describe "Board" do
 
-
-
 		describe '#initialize' do
 			it "creates an empty 6x7 2d array" do
-				expect(subject.board.cells.length).to eql 6
-				expect(subject.board.cells[0].length).to eql 7
+				expect(subject.board.cells.length).to eql 7
+				expect(subject.board.cells[0].length).to eql 6
 				expect(subject.board.cells.flatten.all?{|x| x.nil? }).to eql true
 			end
 		end
@@ -24,7 +22,7 @@ describe ConnectFour do
 				it "returns the cell value" do
 					expect(subject.board.get_cell(2,4)).to eql nil
 					expect(subject.board.get_cell(1,1)).to eql nil
-					subject.board.cells = Array.new(6){Array.new(7, "Red")}
+					subject.board.cells = Array.new(7){Array.new(6, "Red")}
 					expect(subject.board.get_cell(4,5)).to eql "Red"
 				end
 			end
@@ -42,29 +40,53 @@ describe ConnectFour do
 
 			context "given a valid cell coordinate and value" do
 				it "sets the cell to the given value and returns true" do
-					expect(subject.board.set_cell(1,2, "Red")).to eql "Red"
+					expect(subject.board.set_cell(1,2, "Red")).to eql true
 					expect(subject.board.cells[1][2]).to eql "Red"
-					expect(subject.board.set_cell(1,1, "Yellow")).to eql "Yellow"
+					expect(subject.board.set_cell(1,1, "Yellow")).to eql true
 					expect(subject.board.cells[1][1]).to eql "Yellow"
 				end
 			end
 
 			context "given an out of bounds cell coordinate" do
-				it "returns an error message" do
-					expect(subject.board.set_cell(-4, 3, "Yellow")).to eql "Cell (-4, 3) is out of bounds!"
-					expect(subject.board.set_cell(3, 10, "Red")).to eql "Cell (3, 10) is out of bounds!"
+				it "returns false" do
+					expect(subject.board.set_cell(-4, 3, "Yellow")).to eql false
+					expect(subject.board.set_cell(3, 10, "Red")).to eql false
 				end
 			end
 
 			context "given an invalid value" do
-				it "returns an error message" do
-					expect(subject.board.set_cell(1, 2, "Blah")).to eql "Value Blah is invalid!"
-					expect(subject.board.set_cell(3, 4, -1)).to eql "Value -1 is invalid!"
+				it "returns false" do
+					expect(subject.board.set_cell(1, 2, "Blah")).to eql false
+					expect(subject.board.set_cell(3, 4, -1)).to eql false
 				end
 			end
 
 		end
 
+		describe '#count_column' do
+
+			context "given a valid column x coordinate" do
+				it "counts the number of spaces already taken in the specified column" do
+					expect(subject.board.count_column(1)).to eql 0
+					subject.board.set_cell(2,1, "Yellow")
+					subject.board.set_cell(2,2, "Red")
+					subject.board.set_cell(2,3, "Yellow")
+					expect(subject.board.count_column(2)).to eql 3
+				end
+			end
+
+			context "given an invalid column x coordinate" do
+				it "returns an error message" do
+					expect(subject.board.count_column(8)).to eql "Column 8 doesn't exist!"
+					expect(subject.board.count_column(22)).to eql "Column 22 doesn't exist!"
+					expect(subject.board.count_column(-3)).to eql "Column -3 doesn't exist!"
+				end
+			end
+		end
+
+	end
+
+	describe "Player" do
 	end
 
 end
